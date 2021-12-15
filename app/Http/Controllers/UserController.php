072b25b8716f46dd70985909021ba9a4cb9ba9d5
Ternisien Samuel <samuel.ternisien@gmail.com>
2021-12-15 16:56:03 +0100

    user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -20,7 +20,7 @@ class UserController extends Controller
         $lst=[];
         foreach ($user->seen as $el){
             if (!(in_array($el,$lst))){
-                $lst[]=$el;
+                $lst[]=Serie::find($el->serie_id);
             }
         }
 
