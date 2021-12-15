6aefc629c5be6a0e7ed0071a48661dcc0aaca2e1
antoine_bouque <antoine_bouque>
2021-12-15 11:14:22 +0100

    Filtre fix

--- app/Http/Controllers/SerieController.php
+++ app/Http/Controllers/SerieController.php
@@ -25,12 +25,83 @@ class SerieController extends Controller
         $this->series = Serie::all();
     }
 
+    public function index()
+    {
+        $series=Serie::all();
+        return view('welcome',['series'=> $series]);
+    }
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $series=Serie::all();
+        return view('series.index',['series'=> $series]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
 
     public function filtre(Request $request) {
         $genre = $request->get("genre", '');
         $series = [];
         if (empty($genre)) {
-            return view('series.index', ['series' => "test"]);
+            return view('series.index', ['series' => "t"]);
         } else {
             foreach ($this->series as $serie) {
                 if ($serie->genre == $genre) {
