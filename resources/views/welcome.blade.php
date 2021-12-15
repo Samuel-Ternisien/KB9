5841685249edfa12d5af6e585fc414996a3ba2c1
antoine_bouque <antoine_bouque>
2021-12-15 12:10:16 +0100

    Filtre nom & genre

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,31 @@
 @extends('layouts.app')
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Acceuil</title>
+</head>
+<body>
+<h2>KB9</h2>
 @section('content')
     C'est la page générale du site,
     <br />
     on doit y voir les dernières séries par exemple.
 @endsection
+@if(!empty($series))
+    <ul>
+        @foreach($series as $serie)
+            <li>
+                <tr>{{$serie->id}}</tr>
+                <tr>{{$serie->nom}}</tr>
+                <tr>{!!  $serie->resume !!} </tr>
+            </li>
+        @endforeach
+    </ul>
+@else
+    <h3>aucune série</h3>
+@endif
+</body>
+</html>
