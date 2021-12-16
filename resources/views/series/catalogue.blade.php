340d98d359317fed79735638815a5c2f5d09b355
antoine_bouque <antoine_bouque>
2021-12-16 15:03:50 +0100

    Details

--- resources/views/series/catalogue.blade.php
+++ resources/views/series/catalogue.blade.php
@@ -52,7 +52,7 @@
     <select name="" class="selector-genre">
         <option value="">Vous voulez quel style de séries ?</option>
         @foreach($genre as $genres)
-            <option value="{{$genres}}">{{$genres->genre}}</option>
+            <option value="{{$genres}}">{{$genres}}</option>
         @endforeach
     </select>
     <input type="submit" class="submit-search" value="chercher">
