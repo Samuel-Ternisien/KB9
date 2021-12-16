c5be9a8e263f153174d4c8d76f9042c473a51106
antoine_bouque <antoine_bouque>
2021-12-16 13:36:21 +0100

    Details

--- app/Http/Controllers/SerieController.php
+++ app/Http/Controllers/SerieController.php
@@ -51,6 +51,7 @@ class SerieController extends Controller
             foreach (Episode::all() as $episode){
                 $id_episode = DB::table('episodes')->select('id')->where('serie_id', '=', $id_serie)->get();
                 $seen = DB::table('seen')->where('episode_id', '=', $id_episode)->get();
+                echo $seen;
                 if($seen) {
                     return true;
                 }
