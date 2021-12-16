8ef7ebebe43d087fefaed83294779e9be3806b46
Ternisien Samuel <samuel.ternisien@gmail.com>
2021-12-16 18:02:28 +0100

    video

--- resources/views/series/details.blade.php
+++ resources/views/series/details.blade.php
@@ -62,17 +62,22 @@
     <p class="avis-redac-cont">{!! $series[0]-> avis!!}}</p>
 </div>
 @endif
+@if($series[0]->urlAvis!=null)
+    <h2 class="avis-redac">Avis de la rédaction</h2>
+    <iframe style="margin-left: 30%; border-radius: 20px;"width="560" height="315" src="https://www.youtube.com/embed/tvCzmBJvhlI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+@endif
 
 <h2 class="avis-redac">liste des épisodes</h2>
 @for($i=1; $i < $saison_nb+1; $i++)
 <p class="saison-episode-serie">Saison : {{$i}}</p>
-<div class="container-episode-serie" >
 @foreach($episode as $episodes)
     @if($episodes->saison == $i)
 
     <a class="episode-serie"  href="#" style="background-image: url({{'../' . $episodes->urlImage}}">
         <h3 class="episode-serie-nb">épisode {{$episodes->numero}}</h3>
         <h3 class="episode-serie-titre">{{$episodes->nom}}</h3>
+
+        <p></p>
     </a>
 
 
