ed5e58dc812bfde80fa1bc1072fefb22451e3b76
antoine_bouque <antoine_bouque>
2021-12-16 16:02:03 +0100

    Details

--- resources/views/series/catalogue.blade.php
+++ resources/views/series/catalogue.blade.php
@@ -51,7 +51,10 @@
 
 <form action="./series/filtre" method="GET" class="tout-search">
     <input type="text" placeholder="Une idée de série ?" class="search" name="nom">
-    <select name="" class="selector-genre">
+    <input type="submit" class="submit-search" value="chercher">
+</form>
+<form action="./series/filtre" method="GET" class="tout-search">
+    <select name="genre" class="selector-genre">
         <option value="">Vous voulez quel style de séries ?</option>
         @foreach($genres as $genre)
             <option value="{{$genre}}">{{$genre}}</option>
