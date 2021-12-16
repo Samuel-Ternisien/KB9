1703aa91e4af446e75d0af863e913e9a1370f480
antoine_bouque <antoine_bouque>
2021-12-16 11:53:11 +0100

    Details

--- app/Http/Controllers/SerieController.php
+++ app/Http/Controllers/SerieController.php
@@ -66,8 +66,22 @@ class SerieController extends Controller
     }
 
     public function seen($id_episode, $id) {
+        DB::table('seen')->insert([
+            'user_id' => $id,
+            'episode_id' => $id_episode,
+            'date_seen' => now()
+        ]);
 
-        echo $id_episode,$id;
+        $id_serie = DB::table('episodes')->select('serie_id')->where('id', '=', $id_episode)->get();
+
+        foreach ($id_serie as $v){
+            $res = $v->serie_id;
+        }
+
+        return redirect()->action(
+            [SerieController::class, 'serie'], ['id' => $res]
+        );
+
 
     }
 
